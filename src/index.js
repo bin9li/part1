1d1b45cab12bd2a0db1fae82ea532d9e9c64ef32
bin9li <bin9li@foxmail.com>
2020-09-12 21:04:33 +0800

    full_stack 1d1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+//import React from 'react'
+//import ReactDOM from 'react-dom'
+
 /* 
 所有数据仍然耦合在App 组件中，
 让该组件使用props 将必要的数据传递给每个组件。
@@ -54,6 +55,9 @@ const App = () => {
 
 ReactDOM.render(<App />, document.getElementById('root'))
 */
+
+/*
+
 
 const Header = (props) => {
   return(
@@ -111,3 +115,141 @@ const App = () => {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+*/
+/*
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
+*/
+/*
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+*/
+
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+
+
