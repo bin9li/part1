c29f9bc70e88598d98894601583d12ae2ec0cc70
bin9li <bin9li@foxmail.com>
2020-09-12 18:04:11 +0800

    part1bjs

--- src/index.js
+++ src/index.js
@@ -67,8 +67,8 @@ const Content = (props) => {
   return(
     <>
       <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-        {props.parts[1].name} {props.parts[1].exercises}
+        {props.parts[0].name} {props.parts[0].exercises}<br />
+        {props.parts[1].name} {props.parts[1].exercises}<br />
         {props.parts[2].name} {props.parts[2].exercises}
       </p>
     </>
@@ -83,27 +83,29 @@ const Total = (props) => {
 }
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  } 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
