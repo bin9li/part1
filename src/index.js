17d68b075383b90bc2fd1b9bfd2a76630c3f89fa
bin9li <bin9li@foxmail.com>
2020-09-12 16:53:03 +0800

    v0.1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+/* 
+所有数据仍然耦合在App 组件中，
+让该组件使用props 将必要的数据传递给每个组件。
+  Header 负责显示课程的名称
+，Content显示课程的章节及其练习的数量
+，Total 显示练习的总数。
+*/
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = (props) => {
+  return(
+    <>
+      <h1>{props.course}</h1> 
+    </>
+  )
+  
+}
+const Content = (props) => {
+  return(
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  )
+}
+const Total = (props) => {
+  return(
+    <>
+      <p>Number of exercises {props.sum}</p>
+    </>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total sum={exercises1+exercises2+exercises3} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
